f728e2a29d1bac36c249f9db26d646d4355b6655
hiepietel <jachumail@yahoo.pl>
2019-05-22 16:04:42 +0200

    almost done

--- app/src/main/java/com/socketapp/MainActivity.java
+++ app/src/main/java/com/socketapp/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.util.Log;
 import android.widget.TimePicker;
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
     EditText e1;
     TextView tv;
     Button TPbtn;
+    Button magentaBtn, cyanBtn, greenBtn;
+    Switch alarmONOFF;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,10 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
         e1 = (EditText)findViewById(R.id.editText);
         tv = (TextView)findViewById(R.id.textView);
         TPbtn = (Button)findViewById(R.id.TP);
+        cyanBtn = (Button)findViewById(R.id.cyanBtn);
+        magentaBtn = (Button)findViewById(R.id.magentaBtn);
+        greenBtn = (Button)findViewById(R.id.greenBtn);
+        alarmONOFF = (Switch)findViewById(R.id.alarmONOFF);
         TPbtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -31,11 +39,61 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
             }
 
         });
+        magentaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MessageSender messageSender = new MessageSender(MainActivity.this);
+                messageSender.execute("purple");
+            }
+        });
+        cyanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                MessageSender messageSender = new MessageSender(MainActivity.this);
+                messageSender.execute("cyan");
+            }
+        });
+        greenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                MessageSender messageSender = new MessageSender(MainActivity.this);
+                messageSender.execute("green");
+            }
+        });
+        alarmONOFF.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    MessageSender messageSender = new MessageSender(MainActivity.this);
+                    messageSender.execute("alarmOn");
+                }
+                else{
+                    MessageSender messageSender = new MessageSender(MainActivity.this);
+                    messageSender.execute("alarmOff");
+                }
+            }
+        });
     }
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute){
         TextView textView = (TextView)findViewById(R.id.timePicker);
         textView.setText("Hour: "+hourOfDay+ " m: "+minute);
+        String h;
+        if(hourOfDay<10){
+            h = "0"+Integer.toString(hourOfDay);
+        }
+        else {
+            h = Integer.toString(hourOfDay);
+        }
+        String m;
+        if(minute<10){
+            m = "0"+Integer.toString(minute);
+        }
+        else{
+            m = Integer.toString(minute);
+        }
+        MessageSender messageSender = new MessageSender(MainActivity.this);
+        messageSender.execute("sa"+h+"m"+m+"end");
     }
     public void send(View v){
         MessageSender messageSender = new MessageSender(MainActivity.this);
@@ -49,6 +107,9 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
     public void onTaskComplete(String msg) {
         Log.d("msg", "ontask");
         //e1.setText("");
-        tv.setText(msg);
+
+        //String temp = e1.getText() +"\n" +msg;
+
+        tv.setText(msg + "\n"+ tv.getText());
     }
 }
